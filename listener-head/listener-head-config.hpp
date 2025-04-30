06c4309e2806e0e2af270785e938e3e5d27e67f9
Dmitrii Belik <belik820@gmail.com>
2025-04-30 14:29:28 +0200

    fix: listener config

--- listener-head/listener-head-config.hpp
+++ listener-head/listener-head-config.hpp
@@ -52,14 +52,11 @@ struct OverlayInfo {
 
 // Главная структура конфигурации
 struct ListenerConfig {
-  std::vector<ValidatorInfo> validators;
-  std::vector<ValidatorInfo> seed_nodes;
-  std::vector<OverlayInfo> overlays;
-
   // Настройки Listener Head
   int max_connections = 1000;
   int udp_buffer_size = 10 * 1024 * 1024;
   int http_port = 8080;
+  int log_level = 3;
 
   static ListenerConfig from_json(const td::JsonValue &json) {
     ListenerConfig config;
@@ -69,41 +66,15 @@ struct ListenerConfig {
     }
 
     auto &obj = json.get_object();
+    auto listener = td::get_json_object_field(obj, "listener", td::JsonValue::Type::Object, td::JsonValue());
 
-    // Загрузка валидаторов
-    auto validators = td::get_json_object_field(obj, "validators", td::JsonValue::Type::Array, td::JsonValue());
-    if (validators.type() == td::JsonValue::Type::Array) {
-      for (auto &val : validators.get_array()) {
-        if (val.type() == td::JsonValue::Type::Object) {
-          config.validators.push_back(ValidatorInfo::from_json(val));
-        }
-      }
+    if (listener.type() == td::JsonValue::Type::Object) {
+      auto &listener_obj = listener.get_object();
+      config.max_connections = td::get_json_object_int_field(listener_obj, "max_connections", 1000);
+      config.udp_buffer_size = td::get_json_object_int_field(listener_obj, "udp_buffer_size", 10 * 1024 * 1024);
+      config.http_port = td::get_json_object_int_field(listener_obj, "http_port", 8080);
+      config.log_level = td::get_json_object_int_field(listener_obj, "log_level", 3);
     }
-
-    // Загрузка seed-нод
-    auto seeds = td::get_json_object_field(obj, "seed_nodes", td::JsonValue::Type::Array, td::JsonValue());
-    if (seeds.type() == td::JsonValue::Type::Array) {
-      for (auto &seed : seeds.get_array()) {
-        if (seed.type() == td::JsonValue::Type::Object) {
-          config.seed_nodes.push_back(ValidatorInfo::from_json(seed));
-        }
-      }
-    }
-
-    // Загрузка оверлеев
-    auto overlays = td::get_json_object_field(obj, "overlays", td::JsonValue::Type::Array, td::JsonValue());
-    if (overlays.type() == td::JsonValue::Type::Array) {
-      for (auto &overlay : overlays.get_array()) {
-        if (overlay.type() == td::JsonValue::Type::Object) {
-          config.overlays.push_back(OverlayInfo::from_json(overlay));
-        }
-      }
-    }
-
-    // Загрузка настроек
-    config.max_connections = td::get_json_object_int_field(obj, "max_connections", 1000);
-    config.udp_buffer_size = td::get_json_object_int_field(obj, "udp_buffer_size", 10 * 1024 * 1024);
-    config.http_port = td::get_json_object_int_field(obj, "http_port", 8080);
 
     return config;
   }
